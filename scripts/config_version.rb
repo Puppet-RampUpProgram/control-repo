bbb757371683e51fe330f555f470b095ce63ec56
Troy Klein <troy.klein@puppet.com>
2019-08-29 07:16:56 -0500

    added change to config_version

--- scripts/config_version.rb
+++ scripts/config_version.rb
@@ -1,6 +1,7 @@
-#!/usr/bin/env ruby
+#!/opt/puppetlabs/puppet/bin/ruby
 begin
   require 'rugged'
+  require 'socket'
 rescue LoadError
   t = Time.new
   puts t.to_i
@@ -8,12 +9,17 @@ else
   environmentpath = ARGV[0]
   environment     = ARGV[1]
 
+  # Get the hostname of the Puppet master compiling the catalog.
+  # Sometimes the hostname is the fqdn, so we'll take the first segment.
+  compiling_master = Socket.gethostname.split('.').first
+
+  # Get the path to the environment being compiled.
   repo = Rugged::Repository.discover(File.join(environmentpath, environment))
   head = repo.head
 
-  # sha1 hash of the newest commit
-  head_sha = head.target_id
+  # First 12 characters of the sha1 hash of the newest commit.
+  commit_id = head.target_id[0...11]
 
-  # add something to find the remote url
-  puts head_sha
+  # Show the compiling master, environment name, and commit ID.
+  puts "#{compiling_master}-#{environment}-#{commit_id}"
 end
