0199de93234569ef914f5fe3520719c85870c1e3
Troy Klein <troy.klein@puppet.com>
2019-08-30 07:04:15 -0500

    fix for name

--- site/profile/manifests/os/baseline/archives.pp
+++ site/profile/manifests/os/baseline/archives.pp
@@ -1,6 +1,6 @@
 # This profile is to normalize archive for a site
 # it will set temp dir and install dir for the build
-class profile::os::baseline::archives (
+class profile::os::baseline::archive (
   Optional[Stdlib::Absolutepath] $in_temp_path = undef,
 ) {
   include '::archive'
