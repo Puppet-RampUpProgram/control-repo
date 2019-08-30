aa20e4eeb0a7dfbebd60b856d916ccb1a77fccce
Troy Klein <troy.klein@puppet.com>
2019-08-30 05:38:55 -0500

    fixed some lint warnings

--- site/profile/manifests/app/puppet/non_pe_infra_agent.pp
+++ site/profile/manifests/app/puppet/non_pe_infra_agent.pp
@@ -3,7 +3,7 @@ class profile::app::puppet::non_pe_infra_agent (
   String                         $puppet_server = 'puppet.exampledomain.com',
   Stdlib::Absolutepath $path_to_puppet_conf_dir = '/etc/puppetlabs/puppet',
 ) {
-  ini_setting { "puppet server setting":
+  ini_setting { 'puppet server setting':
     ensure  => present,
     path    => "${path_to_puppet_conf_dir}/puppet.conf",
     section => 'main',
