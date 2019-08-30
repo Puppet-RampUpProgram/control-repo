9e8b37344ad8f0d2fb519c6538b04c39ae7b51b9
Troy Klein <troy.klein@puppet.com>
2019-08-30 07:00:29 -0500

    fix for security

--- site/profile/manifests/os/linux/security.pp
+++ site/profile/manifests/os/linux/security.pp
@@ -5,13 +5,11 @@ class profile::os::linux::security {
   include toughen::boot
   include toughen::cron
   include toughen::filesystem
-  include toughen::init
   include toughen::legacy_services
   include toughen::mandatory_access
   include toughen::network
   include toughen::pam
   include toughen::perms_owners
-  include toughen::process
   include toughen::services
   include toughen::shadow
 }
