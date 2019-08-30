12f3fa634fb4675b8fb51a6a4ba5bdbe0fa6a29a
Troy Klein <troy.klein@puppet.com>
2019-08-30 06:42:31 -0500

    added role for masters

--- site/example_role/manifests/sup_svc/puppet/master.pp
+++ site/example_role/manifests/sup_svc/puppet/master.pp
@@ -1,4 +1,5 @@
 # This is a role to add to the Puppet Master.
 class example_role::sup_svc::puppet::master {
-  # include profile::os::baseline
+  include profile::os::baseline
+  include profile::app::puppet::masters
 }
