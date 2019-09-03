5fd0d1ff9c179b90ae7b259da594365f9e9933cd
Troy Klein <troy.klein@puppet.com>
2019-09-03 14:18:09 -0500

    make waring more forward

--- site/profile/manifests/app/puppet/check_hiera_keys.pp
+++ site/profile/manifests/app/puppet/check_hiera_keys.pp
@@ -6,9 +6,12 @@ class profile::app::puppet::check_hiera_keys {
   $hiera_private_key_exists = inline_template("<% if File.exist?('${hiera_private_key}') -%>true<% end -%>")
 
   if $hiera_private_key_exists {
-    $warning_content = "${hiera_private_key} file should be removed from the control repo!\n \
-    Any eyaml encrypted data should be re-encrypted with new keys.  DO NOT PLACE PRIVATE KEY in control-repo!\n \
-    See https://github.com/voxpupuli/hiera-eyaml#generate-keys"
+    $warning_content = "**WARNING** ${hiera_private_key}\n \
+    file should be removed from the control repo!\n \
+    Please generate new eyaml keys for your Puppet Master.\n \
+    Any eyaml encrypted data should be re-encrypted with new keys.\n \
+    DO NOT PLACE PRIVATE KEY in control-repo!\n \
+    See https://github.com/voxpupuli/hiera-eyaml#generate-keys \n"
 
     warning($warning_content)
     notify { 'key error':
