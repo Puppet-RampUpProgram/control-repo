0edada86eec6f067995efec3814c44f3708b4f7e
Troy Klein <troy.klein@puppet.com>
2019-09-03 13:52:17 -0500

    testing other toughen classes on PE Master

--- site/profile/manifests/os/linux/security.pp
+++ site/profile/manifests/os/linux/security.pp
@@ -2,13 +2,13 @@
 class profile::os::linux::security {
   include toughen::banners
   include toughen::boot
-  #include toughen::cron
+  include toughen::cron
+  include toughen::perms_owners
+  include toughen::shadow
   #include toughen::auditing
   #include toughen::legacy_services
   #include toughen::mandatory_access
   #include toughen::network
   #include toughen::pam
-  #include toughen::perms_owners
   #include toughen::services
-  include toughen::shadow
 }
