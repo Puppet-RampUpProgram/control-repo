e5f5d0f8523892c81a79b0f00d9e409248ca83ca
Troy Klein <troy.klein@puppet.com>
2019-09-04 11:04:38 -0500

    fix for fastb

--- site/profile/manifests/app/fastb.pp
+++ site/profile/manifests/app/fastb.pp
@@ -1,17 +1,19 @@
 # This is a example profile to deploy fastb application software.
 class profile::app::fastb (
   Stdlib::HTTPSUrl $download_url = 'https://tomcat.apache.org/tomcat-9.0-doc/appdev/sample/sample.war',
-  Stdlib::Absolutepath $app_path = '/opt/tomcat/fastb',
+  Stdlib::Absolutepath $app_path = 'fastb',
 ) {
   include profile::app::tomcat::webserver
 
+  $catalina_home = $profile::app::tomcat::webserver::tomcat_install_path
+
   tomcat::instance { 'tomcat8-fastb':
-    catalina_home => $app_path,
-    catalina_base => $app_path,
+    catalina_home => $catalina_home,
+    catalina_base => "${catalina_home}/${app_path}",
   }
 
-  tomcat::war { "${app_path}/fastb_app.war":
-    catalina_base => $app_path,
+  tomcat::war { "${catalina_home}/${app_path}/fastb_app.war":
+    catalina_base => "${catalina_home}/${app_path}",
     war_source    => $download_url,
   }
 
@@ -27,7 +29,7 @@ class profile::app::fastb (
 #  }
 
   tomcat::config::server::connector { 'tomcat-fastb-http':
-    catalina_base => $app_path,
+    catalina_base => "${catalina_home}/${app_path}",
     port          => '8081',
     protocol      => 'HTTP/1.1',
 #    additional_attributes => {
