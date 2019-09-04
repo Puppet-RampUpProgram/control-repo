eeb22e1eaaa1b5a0da1a21d42f6ade93e5c85e85
Troy Klein <troy.klein@puppet.com>
2019-09-04 16:59:37 -0500

    fastb with tomcat::war

--- site/profile/manifests/app/fastb.pp
+++ site/profile/manifests/app/fastb.pp
@@ -5,7 +5,15 @@ class profile::app::fastb (
 ) {
   include profile::app::tomcat::webserver
 
+  $user           = $profile::app::tomcat::webserver::user
+  $group          = $profile::app::tomcat::webserver::group
+  $service        = $profile::app::tomcat::webserver::service
   $catalina_home = $profile::app::tomcat::webserver::tomcat_install_path
 
-
+  tomcat::war { 'http-demo.war':
+    war_source => $download_url,
+    user       => $user,
+    group      => $group,
+    notify     => Service['service'],
+  }
 }
