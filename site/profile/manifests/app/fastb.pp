f070223bf60523ff14149f7e6019b59475903bb4
Troy Klein <troy.klein@puppet.com>
2019-09-04 10:54:13 -0500

    test for fastb web_be

--- site/profile/manifests/app/fastb.pp
+++ site/profile/manifests/app/fastb.pp
@@ -6,7 +6,7 @@ class profile::app::fastb (
   include profile::app::tomcat::webserver
 
   tomcat::instance { 'tomcat8-fastb':
-    catalina_home => "${app_path}/..",
+    catalina_home => $app_path,
     catalina_base => $app_path,
   }
 
@@ -15,24 +15,24 @@ class profile::app::fastb (
     war_source    => $download_url,
   }
 
-  tomcat::instance { 'tomcat-second':
-    catalina_home => '/opt/tomcat',
-    catalina_base => '/opt/tomcat/second',
-  }
+#  tomcat::instance { 'tomcat-second':
+#    catalina_home => '/opt/tomcat',
+#   catalina_base => '/opt/tomcat/second',
+#  }
 
   # Change the default port of the second instance server and HTTP connector
-  tomcat::config::server { 'tomcat-second':
-    catalina_base => '/opt/tomcat/second',
-    port          => '8006',
-  }
+#  tomcat::config::server { 'tomcat-second':
+#    catalina_base => '/opt/tomcat/second',
+#    port          => '8006',
+#  }
 
-  tomcat::config::server::connector { 'tomcat-second-http':
-    catalina_base         => '/opt/tomcat/second',
-    port                  => '8081',
-    protocol              => 'HTTP/1.1',
-    additional_attributes => {
-      'redirectPort' => '8443'
-    },
+  tomcat::config::server::connector { 'tomcat-fastb-http':
+    catalina_base => $app_path,
+    port          => '8081',
+    protocol      => 'HTTP/1.1',
+#    additional_attributes => {
+#      'redirectPort' => '8443'
+#    },
   }
 
 }
