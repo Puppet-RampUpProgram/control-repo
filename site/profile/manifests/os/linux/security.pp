c914fd5d90e0b5a1e0b923a44e598ec8092ce155
Troy Klein <troy.klein@puppet.com>
2019-09-04 09:29:38 -0500

    added $pe_environment to profile

--- site/profile/manifests/os/linux/security.pp
+++ site/profile/manifests/os/linux/security.pp
@@ -1,4 +1,8 @@
 # This is a class example for security
-class profile::os::linux::security {
-  include os_hardening
+class profile::os::linux::security (
+  Boolean $pe_environment = true,
+) {
+  class { '::os_hardening':
+    pe_environment => $pe_environment,
+  }
 }
