744b15a3a9d97ac17866302ff8c0e37ec601aa1b
Troy Klein <troy.klein@puppet.com>
2019-09-05 10:44:08 -0500

    pass mysql_user and password_hash

--- site/profile/manifests/app/mysql/server.pp
+++ site/profile/manifests/app/mysql/server.pp
@@ -3,7 +3,8 @@
 class profile::app::mysql::server (
   # root_password is set in hiera data based on role and env_n_role
   #  Please delete files in "<control-repo/data/**/*.yaml"
-  Variant[String, Sensitive[String]] $root_password,
+  #Variant[String, Sensitive[String]] $root_password,
+  String $root_password,
   Array[String] $mysql_bindings = [ 'php' ],
   Hash[String, Hash[String, Any]] $dbs = {},
 ) {
@@ -15,6 +16,14 @@ class profile::app::mysql::server (
                                 },
   } )
   assert_type(Hash[String, Any], $lookup_settings)
+
+  $lookup_mysql_users = lookup( { 'name' => 'profile::app::mysql::server::mysql_users',
+                                'merge' => {
+                                  'strategy' => 'deep',
+                                  'knockout_prefix' => '--',
+                                },
+  } )
+  assert_type(Hash[Hash, Any], $lookup_mysql_users)
 
   # This will ensure the root_password is of Sensitive datatype to protect the 
   # root_password from showing up in the logs.  
@@ -30,11 +39,19 @@ class profile::app::mysql::server (
     root_password => $secure_root_pass,
       *           => $lookup_settings,
   }
+  contain mysql::server
 
   $mysql_bindings.each | String $binding | {
     contain "mysql::bindings::${binding}"
   }
-  contain mysql::server
+
+  $lookup_mysql_users.each |String $username, Hash $user_opts| {
+    $password_hash = mysql_password(lookup("profile::app::mysql::server::${username}::password"))
+    mysql_user { $username:
+      password_hash => $password_hash,
+      *             => $user_opts,
+    }
+  }
 
   $dbs.each |$dbname, $opts| {
     mysql::db { $dbname:
