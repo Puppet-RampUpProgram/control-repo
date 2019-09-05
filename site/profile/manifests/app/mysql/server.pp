13ca7fcf48e140157ad899198d73f5c8d90ae9ce
Troy Klein <troy.klein@puppet.com>
2019-09-05 09:21:13 -0500

    bug fix

--- site/profile/manifests/app/mysql/server.pp
+++ site/profile/manifests/app/mysql/server.pp
@@ -13,14 +13,14 @@ class profile::app::mysql::server (
                                   'stratagy' => 'deep',
                                   'knockout_prefix' => '--',
                                 },
-  })
+  } )
   assert_type(Hash[String, Any], $lookup_settings)
 
-  }
   class {  'mysql::server':
     root_password => $root_password,
       *           => $lookup_settings,
   }
+
   $mysql_bindings.each | String $binding | {
     contain "mysql::bindings::${binding}"
   }
@@ -32,40 +32,4 @@ class profile::app::mysql::server (
     }
   }
 
-  # @summary Install the MySQL database server
-#
-# @see https://forge.puppet.com/puppetlabs/mysql
-#
-# @example Basic usage
-#   include r_profile::database::mysql_server
-#
-# @example Server settings
-#   r_profile::database::mysql_server::settings:
-#     root_password: "TopSecr3t"
-#     remove_default_accounts: true
-#
-# @example Database creation
-#   r_profile::database::mysql_server::dbs:
-#     'mydb':
-#       user: 'myuser'
-#       password: 'mypass'
-#       host: 'localhost'
-#       grant:
-#         - 'SELECT'
-#         - 'UPDATE'
-#
-# @param settings Hash of server settings to enforce (see examples)
-# @param dbs Hash of databases to create (see examples)
-
-  class { 'mysql::server':
-    * => $settings,
-  }
-
-  $dbs.each |$key, $opts| {
-    mysql::db { $key:
-      * => $opts,
-    }
-  }
-
-}
 }
